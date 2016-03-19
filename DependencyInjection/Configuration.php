4c0ad80318c5b27dd5e20762cd88c07dfc5c64c7
Cristian <cristianpana86@yahoo.com>
2016-03-20 00:36:03 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -24,21 +24,21 @@ class Configuration implements ConfigurationInterface
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
         $rootNode
-            ->children()
-			  ->arrayNode('group_hierarchy_manager_neo4j')
-                    ->children()
-						->scalarNode('neo4j_user')->defaultValue('')->end()
-						->scalarNode('neo4j_password')->defaultValue('')->end()
-						->scalarNode('def_rel_type_group_to_group')->defaultValue('')->end()
-						->scalarNode('def_rel_type_user_to_group')->defaultValue('')->end()
-						->scalarNode('root_group_id')->defaultValue('')->end()
-						->scalarNode('manager_role_property')->defaultValue('')->end()
-						->scalarNode('default_property_group')->defaultValue('')->end()
-						->scalarNode('default_property_user')->defaultTrue()->end()
-				    ->end()
-                ->end() // group_hierarchy_manager_neo4j
-            ->end()
-            ;
+                ->children()
+			->arrayNode('group_hierarchy_manager_neo4j')
+                                ->children()
+				        ->scalarNode('neo4j_user')->defaultValue('')->end()
+					->scalarNode('neo4j_password')->defaultValue('')->end()
+					->scalarNode('def_rel_type_group_to_group')->defaultValue('')->end()
+					->scalarNode('def_rel_type_user_to_group')->defaultValue('')->end()
+					->scalarNode('root_group_id')->defaultValue('')->end()
+					->scalarNode('manager_role_property')->defaultValue('')->end()
+					->scalarNode('default_property_group')->defaultValue('')->end()
+					->scalarNode('default_property_user')->defaultTrue()->end()
+				->end()
+                       ->end() // group_hierarchy_manager_neo4j
+                ->end()
+                ;
 
         return $treeBuilder;
     }
